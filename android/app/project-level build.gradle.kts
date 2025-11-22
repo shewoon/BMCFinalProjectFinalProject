95881223905164f90add656440c8e37d9a24315d
shewoon <168503953+shewoon@users.noreply.github.com>
2025-11-22 23:48:12 +0800

    Flutter 3.32.5V Firebase Integration Final Project

---
+++ android/app/project-level build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.google.gms:google-services:4.4.1") // FlutterFire
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
